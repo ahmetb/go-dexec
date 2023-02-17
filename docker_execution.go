110fd77215de43bdb9fd0fce1f4dd040747bd0b5
austinarbor-wk <austin.arbor@workiva.com>
2023-02-17 15:31:29 -0500

    move execution type to separate file, bound the possible types

--- docker_execution.go
+++ docker_execution.go
@@ -8,19 +8,6 @@ import (
 	"github.com/fsouza/go-dockerclient"
 )
 
-// Execution determines how the command is going to be executed. Currently
-// the only method is ByCreatingContainer.
-type Execution[T any] interface {
-	create(d T, cmd []string) error
-	run(d T, stdin io.Reader, stdout, stderr io.Writer) error
-	wait(d T) (int, error)
-
-	setEnv(env []string) error
-	setDir(dir string) error
-	getID() string
-	kill(d T) error
-}
-
 type createContainer struct {
 	opt docker.CreateContainerOptions
 	cmd []string
---
+++ execution.go
@@ -0,0 +1,20 @@
+package dexec
+
+import "io"
+
+type ContainerClient interface {
+	Docker | ContainerD
+}
+
+// Execution determines how the command is going to be executed. Currently
+// the only method is ByCreatingContainer.
+type Execution[T ContainerClient] interface {
+	create(d T, cmd []string) error
+	run(d T, stdin io.Reader, stdout, stderr io.Writer) error
+	wait(d T) (int, error)
+
+	setEnv(env []string) error
+	setDir(dir string) error
+	getID() string
+	kill(d T) error
+}
