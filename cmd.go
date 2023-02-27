6642032d0194951f3ddc20304c1dd7307ab0e081
austinarbor-wk <austin.arbor@workiva.com>
2023-02-27 15:27:31 -0500

    consolidate the duplicate code into the GenericCmd struct

--- cmd.go
+++ cmd.go
@@ -1,25 +1,74 @@
 package dexec
 
 import (
+	"bytes"
+	"errors"
 	"io"
+	"io/ioutil"
 )
 
 type Cmd interface {
+	// StdoutPipe returns a pipe that will be connected to the command's standard output when
+	// the command starts.
+	//
+	// Wait will close the pipe after seeing the command exit or in error conditions.
 	StdoutPipe() (io.ReadCloser, error)
+	// StderrPipe returns a pipe that will be connected to the command's standard error when
+	// the command starts.
+	//
+	// Wait will close the pipe after seeing the command exit or in error conditions.
 	StderrPipe() (io.ReadCloser, error)
+	// SetStderr sets the stderr writer
 	SetStderr(writer io.Writer)
+	// StdinPipe returns a pipe that will be connected to the command's standard input
+	// when the command starts.
+	//
+	// Different than os/exec.StdinPipe, returned io.WriteCloser should be closed by user.
+	StdinPipe() (io.WriteCloser, error)
+	// GetPID will return a unique identifier for the running command. The meaning of the identifier
+	// may be implementation specific
 	GetPID() string
+	// Start starts the specified command but does not wait for it to complete.
 	Start() error
+	// Wait waits for the command to exit. It must have been started by Start.
+	//
+	// If the container exits with a non-zero exit code, the error is of type
+	// *ExitError. Other error types may be returned for I/O problems and such.
+	//
+	// Different than os/exec.Wait, this method will not release any resources
+	// associated with Cmd (such as file handles).
 	Wait() error
+	// Kill will stop a running command
 	Kill() error
+	// Run starts the specified command and waits for it to complete.
+	//
+	// If the command runs successfully and copying streams are done as expected,
+	// the error is nil.
+	//
+	// If the container exits with a non-zero exit code, the error is of type
+	// *ExitError. Other error types may be returned for I/O problems and such.
 	Run() error
+	// Output runs the command and returns its standard output.
+	//
+	// If the container exits with a non-zero exit code, the error is of type
+	// *ExitError. Other error types may be returned for I/O problems and such.
+	//
+	// If c.Stderr was nil, Output populates ExitError.Stderr.
 	Output() ([]byte, error)
+	// CombinedOutput runs the command and returns its combined standard output and
+	// standard error.
+	//
+	// Docker API does not have strong guarantees over ordering of messages. For instance:
+	//     >&1 echo out; >&2 echo err
+	// may result in "out\nerr\n" as well as "err\nout\n" from this method.
 	CombinedOutput() ([]byte, error)
+	// SetDir sets the working directory for the command
 	SetDir(dir string)
+	// Cleanup cleans up any resources that were created for the command
 	Cleanup() error
 }
 
-type GenericCmd struct {
+type GenericCmd[T ContainerClient] struct {
 	// Path is the path or name of the command in the container.
 	Path string
 
@@ -50,7 +99,173 @@ type GenericCmd struct {
 	Stdout         io.Writer
 	Stderr         io.Writer
 	started        bool
+	Method         Execution[T]
 	closeAfterWait []io.Closer
+	client         T
+}
+
+func (g *GenericCmd[T]) Start() error {
+	if g.Dir != "" {
+		if err := g.Method.setDir(g.Dir); err != nil {
+			return err
+		}
+	}
+	if g.Env != nil {
+		if err := g.Method.setEnv(g.Env); err != nil {
+			return err
+		}
+	}
+
+	if g.started {
+		return errors.New("dexec: already started")
+	}
+	g.started = true
+
+	if g.Stdin == nil {
+		g.Stdin = empty
+	}
+	if g.Stdout == nil {
+		g.Stdout = ioutil.Discard
+	}
+	if g.Stderr == nil {
+		g.Stderr = ioutil.Discard
+	}
+
+	cmd := append([]string{g.Path}, g.Args...)
+	if err := g.Method.create(g.client, cmd); err != nil {
+		return err
+	}
+	if err := g.Method.run(g.client, g.Stdin, g.Stdout, g.Stderr); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (g *GenericCmd[T]) CombinedOutput() ([]byte, error) {
+	if g.Stdout != nil {
+		return nil, errors.New("dexec: Stdout already set")
+	}
+	if g.Stderr != nil {
+		return nil, errors.New("dexec: Stderr already set")
+	}
+	var b bytes.Buffer
+	g.Stdout, g.Stderr = &b, &b
+	err := g.Run()
+	return b.Bytes(), err
+}
+
+func (g *GenericCmd[T]) Run() error {
+	if err := g.Start(); err != nil {
+		return err
+	}
+	return g.Wait()
+}
+
+func (g *GenericCmd[T]) Wait() error {
+	defer closeFds(g.closeAfterWait)
+	if !g.started {
+		return errors.New("dexec: not started")
+	}
+	ec, err := g.Method.wait(g.client)
+	if err != nil {
+		return err
+	}
+	if ec != 0 {
+		return &ExitError{ExitCode: ec}
+	}
+	return nil
+}
+
+func (g *GenericCmd[T]) Output() ([]byte, error) {
+	if g.Stdout != nil {
+		return nil, errors.New("dexec: Stdout already set")
+	}
+	var stdout, stderr bytes.Buffer
+	g.Stdout = &stdout
+
+	captureErr := g.Stderr == nil
+	if captureErr {
+		g.Stderr = &stderr
+	}
+	err := g.Run()
+	if err != nil && captureErr {
+		if ee, ok := err.(*ExitError); ok {
+			ee.Stderr = stderr.Bytes()
+		}
+	}
+	return stdout.Bytes(), err
+}
+
+// StdinPipe returns a pipe that will be connected to the command's standard input
+// when the command starts.
+//
+// Different than os/exec.StdinPipe, returned io.WriteCloser should be closed by user.
+func (g *GenericCmd[T]) StdinPipe() (io.WriteCloser, error) {
+	if g.Stdin != nil {
+		return nil, errors.New("dexec: Stdin already set")
+	}
+	pr, pw := io.Pipe()
+	g.Stdin = pr
+	return pw, nil
+}
+
+// StdoutPipe returns a pipe that will be connected to the command's standard output when
+// the command starts.
+//
+// Wait will close the pipe after seeing the command exit or in error conditions.
+func (g *GenericCmd[T]) StdoutPipe() (io.ReadCloser, error) {
+	if g.Stdout != nil {
+		return nil, errors.New("dexec: Stdout already set")
+	}
+	pr, pw := io.Pipe()
+	g.Stdout = pw
+	g.closeAfterWait = append(g.closeAfterWait, pw)
+	return pr, nil
+}
+
+// StderrPipe returns a pipe that will be connected to the command's standard error when
+// the command starts.
+//
+// Wait will close the pipe after seeing the command exit or in error conditions.
+func (g *GenericCmd[T]) StderrPipe() (io.ReadCloser, error) {
+	if g.Stderr != nil {
+		return nil, errors.New("dexec: Stderr already set")
+	}
+	pr, pw := io.Pipe()
+	g.Stderr = pw
+	g.closeAfterWait = append(g.closeAfterWait, pw)
+	return pr, nil
+}
+
+// GetPID will return the container ID of the Cmd's running container.  This is useful
+// for when we need to cleanup the process before completion or store its container ID
+func (g *GenericCmd[T]) GetPID() string {
+	if g.started {
+		return g.Method.getID()
+	}
+
+	return ""
+}
+
+func (g *GenericCmd[T]) SetDir(dir string) {
+	g.Dir = dir
+}
+
+func (g *GenericCmd[T]) SetStderr(writer io.Writer) {
+	g.Stderr = writer
+}
+
+// Kill will stop a running container
+func (g *GenericCmd[T]) Kill() error {
+	if g.started {
+		return g.Method.kill(g.client)
+	}
+
+	return nil
+}
+
+func (g *GenericCmd[T]) Cleanup() error {
+	return g.Method.cleanup(g.client)
 }
 
 func closeFds(l []io.Closer) {
--- containerd.go
+++ containerd.go
@@ -1,11 +1,7 @@
 package dexec
 
 import (
-	"bytes"
-	"errors"
 	"github.com/containerd/containerd"
-	"io"
-	"io/ioutil"
 )
 
 type ContainerD struct {
@@ -14,208 +10,16 @@ type ContainerD struct {
 }
 
 type ContainerDCmd struct {
-	GenericCmd
-	Method     Execution[ContainerD]
-	containerD ContainerD
+	*GenericCmd[ContainerD]
 }
 
 func (c ContainerD) Command(method Execution[ContainerD], name string, arg ...string) *ContainerDCmd {
 	return &ContainerDCmd{
-		GenericCmd: GenericCmd{
-			Path: name,
-			Args: arg,
+		GenericCmd: &GenericCmd[ContainerD]{
+			Path:   name,
+			Args:   arg,
+			Method: method,
+			client: c,
 		},
-		Method:     method,
-		containerD: c,
 	}
-}
-
-func (c *ContainerDCmd) Start() error {
-	if c.Dir != "" {
-		if err := c.Method.setDir(c.Dir); err != nil {
-			return err
-		}
-	}
-	if c.Env != nil {
-		if err := c.Method.setEnv(c.Env); err != nil {
-			return err
-		}
-	}
-
-	if c.started {
-		return errors.New("dexec: already started")
-	}
-	c.started = true
-
-	if c.Stdin == nil {
-		c.Stdin = empty
-	}
-	if c.Stdout == nil {
-		c.Stdout = ioutil.Discard
-	}
-	if c.Stderr == nil {
-		c.Stderr = ioutil.Discard
-	}
-
-	cmd := append([]string{c.Path}, c.Args...)
-	if err := c.Method.create(c.containerD, cmd); err != nil {
-		return err
-	}
-	if err := c.Method.run(c.containerD, c.Stdin, c.Stdout, c.Stderr); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Wait waits for the command to exit. It must have been started by Start.
-//
-// If the container exits with a non-zero exit code, the error is of type
-// *ExitError. Other error types may be returned for I/O problems and such.
-//
-// Different than os/exec.Wait, this method will not release any resources
-// associated with Cmd (such as file handles).
-func (c *ContainerDCmd) Wait() error {
-	defer closeFds(c.closeAfterWait)
-	if !c.started {
-		return errors.New("dexec: not started")
-	}
-	ec, err := c.Method.wait(c.containerD)
-	if err != nil {
-		return err
-	}
-	if ec != 0 {
-		return &ExitError{ExitCode: ec}
-	}
-	return nil
-}
-
-// Run starts the specified command and waits for it to complete.
-//
-// If the command runs successfully and copying streams are done as expected,
-// the error is nil.
-//
-// If the container exits with a non-zero exit code, the error is of type
-// *ExitError. Other error types may be returned for I/O problems and such.
-func (c *ContainerDCmd) Run() error {
-	if err := c.Start(); err != nil {
-		return err
-	}
-	return c.Wait()
-}
-
-// CombinedOutput runs the command and returns its combined standard output and
-// standard error.
-//
-// Docker API does not have strong guarantees over ordering of messages. For instance:
-//     >&1 echo out; >&2 echo err
-// may result in "out\nerr\n" as well as "err\nout\n" from this method.
-func (c *ContainerDCmd) CombinedOutput() ([]byte, error) {
-	if c.Stdout != nil {
-		return nil, errors.New("dexec: Stdout already set")
-	}
-	if c.Stderr != nil {
-		return nil, errors.New("dexec: Stderr already set")
-	}
-	var b bytes.Buffer
-	c.Stdout, c.Stderr = &b, &b
-	err := c.Run()
-	return b.Bytes(), err
-}
-
-// Output runs the command and returns its standard output.
-//
-// If the container exits with a non-zero exit code, the error is of type
-// *ExitError. Other error types may be returned for I/O problems and such.
-//
-// If c.Stderr was nil, Output populates ExitError.Stderr.
-func (c *ContainerDCmd) Output() ([]byte, error) {
-	if c.Stdout != nil {
-		return nil, errors.New("dexec: Stdout already set")
-	}
-	var stdout, stderr bytes.Buffer
-	c.Stdout = &stdout
-
-	captureErr := c.Stderr == nil
-	if captureErr {
-		c.Stderr = &stderr
-	}
-	err := c.Run()
-	if err != nil && captureErr {
-		if ee, ok := err.(*ExitError); ok {
-			ee.Stderr = stderr.Bytes()
-		}
-	}
-	return stdout.Bytes(), err
-}
-
-// StdinPipe returns a pipe that will be connected to the command's standard input
-// when the command starts.
-//
-// Different than os/exec.StdinPipe, returned io.WriteCloser should be closed by user.
-func (c *ContainerDCmd) StdinPipe() (io.WriteCloser, error) {
-	if c.Stdin != nil {
-		return nil, errors.New("dexec: Stdin already set")
-	}
-	pr, pw := io.Pipe()
-	c.Stdin = pr
-	return pw, nil
-}
-
-// StdoutPipe returns a pipe that will be connected to the command's standard output when
-// the command starts.
-//
-// Wait will close the pipe after seeing the command exit or in error conditions.
-func (c *ContainerDCmd) StdoutPipe() (io.ReadCloser, error) {
-	if c.Stdout != nil {
-		return nil, errors.New("dexec: Stdout already set")
-	}
-	pr, pw := io.Pipe()
-	c.Stdout = pw
-	c.closeAfterWait = append(c.closeAfterWait, pw)
-	return pr, nil
-}
-
-// StderrPipe returns a pipe that will be connected to the command's standard error when
-// the command starts.
-//
-// Wait will close the pipe after seeing the command exit or in error conditions.
-func (c *ContainerDCmd) StderrPipe() (io.ReadCloser, error) {
-	if c.Stderr != nil {
-		return nil, errors.New("dexec: Stderr already set")
-	}
-	pr, pw := io.Pipe()
-	c.Stderr = pw
-	c.closeAfterWait = append(c.closeAfterWait, pw)
-	return pr, nil
-}
-
-// GetPID will return the container ID of the Cmd's running container.  This is useful
-// for when we need to cleanup the process before completion or store its container ID
-func (c *ContainerDCmd) GetPID() string {
-	if c.started {
-		return c.Method.getID()
-	}
-
-	return ""
-}
-
-func (c *ContainerDCmd) SetDir(dir string) {
-	c.Dir = dir
-}
-
-func (c *ContainerDCmd) SetStderr(writer io.Writer) {
-	c.Stderr = writer
-}
-
-// Kill will stop a running container
-func (c *ContainerDCmd) Kill() error {
-	if c.started {
-		return c.Method.kill(c.containerD)
-	}
-
-	return nil
-}
-
-func (c *ContainerDCmd) Cleanup() error {
-	return c.Method.cleanup(c.containerD)
 }
